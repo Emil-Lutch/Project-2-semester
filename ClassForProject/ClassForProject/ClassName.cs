7f5ec143d5ac904dd0b36736a26fb5bffa3b2dd1
Emil-Lutch <ajdarovaleks@gmail.com>
2026-05-08 09:52:28 +0300

    Add files via upload

---
+++ ClassForProject/ClassForProject/ClassName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassForProject
+{
+    internal class ClassName
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public ClassName(string name, string surname) 
+        {
+            Name = name;
+            Surname = surname;
+        }
+        public string GetInfoName()
+        {
+            return $"{Name} {Surname}";
+        }
+    }
+}
---
+++ ClassForProject/ClassForProject/ClassNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassForProject
+{
+    internal class ClassNumber
+    {
+        public string Number { get; set; }
+        public ClassName Contact {  get; set; }
+        public ClassNumber(string number, ClassName contact) 
+        {
+            Number = number;
+            Contact = contact;
+        }
+        public void GetFullInfo()
+        {
+            Console.WriteLine($"Name: {Contact.GetInfoName()}");
+            Console.WriteLine($"Number: {Number}");
+        }
+    }
+}
