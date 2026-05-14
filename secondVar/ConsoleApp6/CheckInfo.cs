9ebf582da896fef23c875c42b0c32608f9ee65da
Emil-Lutch <ajdarovaleks@gmail.com>
2026-05-14 09:35:18 +0300

    Add files via upload

---
+++ secondVar/ConsoleApp6/CheckInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class CheckInfo
+    {
+        public List<Contact> Contacts = new List<Contact>();
+        public void AddCon(Contact contact)
+        {
+            Contacts.Add(contact);
+        }
+        public void RemoveCon(string number)
+        {
+            Contacts.RemoveAll(contact  => contact.Number == number); 
+        }
+        public void ShowAllCon()
+        {
+            
+        }
+    }
+}
